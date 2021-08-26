7c7cec68bbe57890c81bb579a01505575638812c
yoshiki0756 <g18lb014.yoshiki@gmail.com>
2021-08-26 18:29:06 +0900

    commit -m"[forst comiit]"

---
+++ pleiades/workspace/JavaWcp/src/Question1_1.java
@@ -0,0 +1,15 @@
+//課題1-1）　1から100までの整数を全て足す計算プログラムを作成してください
+
+public class Question1_1 {
+	public static void main(String[] args) {
+		
+		int number = 100;
+		int sum = 0;
+		
+		for(int i = 1; i <= number; i++) {
+			sum = sum + i;
+		}
+		System.out.println("1から100までの整数を全て足した和は" + sum);
+	}
+
+}
